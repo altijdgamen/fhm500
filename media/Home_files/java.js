f72d5cf0e56389c30bb05b771e17cd2ce88cd5b7
altijdgamen <87279@glr.nl>
2022-06-03 16:20:26 +0200

    index

---
+++ media/Home_files/java.js
@@ -0,0 +1,75 @@
+let prijs = 0;
+let naam = "";
+
+
+let naam1 = "";
+let naam2 = "";
+let naam3 = "";
+let naam4 = "";
+let naam5 = "";
+
+totaalbedragdiv = document.getElementById("totaal")
+totaalAantalNaam = document.getElementById("naam")
+
+let totaalbedrag1 = 0;
+let totaalbedrag2 = 0;
+let totaalbedrag3 = 0;
+let totaalbedrag4 = 0;
+let totaalbedrag5 = 0;
+
+
+function vrouw1()
+{
+    let naam = "Juultje"
+    let prijs = 250;
+    totaalbedrag1 = prijs;
+    naam1 = naam;
+    
+    bekijkTotaal();
+}
+function vrouw2()
+{
+    let naam = "Olivia";
+    let prijs = 200;
+    totaalbedrag2 = prijs;
+    naam2 = naam
+    bekijkTotaal();
+}
+function vrouw3()
+{
+    let naam = "adison"
+    let prijs = 250;
+    totaalbedrag3 = prijs;
+    naam3 = naam
+    bekijkTotaal();
+}
+function vrouw4()
+{
+    let prijs = 250;
+    totaalbedrag4 = prijs;
+    bekijkTotaal();
+}
+function vrouw5()
+{
+    let prijs = 250;
+    totaalbedrag5 = prijs;
+    bekijkTotaal();
+}
+
+
+
+
+
+
+
+function bekijkTotaal()
+{
+    console.log("hallo")
+    let totaal = totaalbedrag1 + totaalbedrag2 + totaalbedrag3;
+    totaalbedragdiv.innerHTML = totaal;
+    let aantalNaam = naam1 + ", " + naam2 + ", " + naam3 ; 
+    totaalAantalNaam.innerHTML = "je hebt de volgende NFT'S gekocht: " + aantalNaam + ","
+    
+    
+
+}
