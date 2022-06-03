1594c09b58b78ef09ede2a223d942efe20efc67e
altijdgamen <87279@glr.nl>
2022-06-03 14:43:33 +0200

    divs aangemaakt en prijs berekening aangemaakt

---
+++ javascript/java.js
@@ -0,0 +1,53 @@
+let prijs = 0;
+let aantal = 0;
+
+totaalbedragdiv = document.getElementById("totaal")
+let totaalbedrag1 = 0;
+let totaalbedrag2 = 0;
+let totaalbedrag3 = 0;
+let totaalbedrag4 = 0;
+let totaalbedrag5 = 0;
+
+
+function vrouw1()
+{
+    let prijs = 250;
+    totaalbedrag1 = prijs;
+    bekijkTotaal();
+}
+function vrouw2()
+{
+    let prijs = 250;
+    totaalbedrag2 = prijs;
+    bekijkTotaal();
+}
+function vrouw3()
+{
+    let prijs = 250;
+    totaalbedrag3 = prijs;
+    bekijkTotaal();
+}
+function vrouw4()
+{
+    let prijs = 250;
+    totaalbedrag4 = prijs;
+    bekijkTotaal();
+}
+function vrouw5()
+{
+    let prijs = 250;
+    totaalbedrag5 = prijs;
+    bekijkTotaal();
+}
+
+
+
+
+
+
+
+function bekijkTotaal()
+{
+    let totaal = totaalbedrag1 
+    totaalbedragdiv.innerHTML = totaal
+}
